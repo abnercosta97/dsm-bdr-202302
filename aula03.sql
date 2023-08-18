5bc6b2a6a9ef42144e93db674a2307cd444b7115
Abner Rodrigo <127696147+abnercosta97@users.noreply.github.com>
2023-08-17 21:00:38 -0300

    Update aula03.sql

--- aula03.sql
+++ aula03.sql
@@ -1,28 +1,13 @@
-drop database IF EXISTS bd_aula03;
+DROP DATABASE IF EXISTS bd_aula03;
 
-create database bd_aula03;
-
+CREATE DATABASE bd_aula03;
 \c bd_aula03;
 
-create table tbl_cliente(codigo_cliente INTEGER PRIMARY KEY, Nome NOT NULL, Cidade text, Endereco text);
+CREATE TABLE tbl_cliente (codigo_cliente SERIAL PRIMARY KEY, Nome TEXT NOT NULL,Cidade TEXT,Endereco TEXT);
 
-create table tbl_titulo(codigo_titulo INTEGER PRIMARY KEY, titulo NOT NULL, descricao text, categoria IN(DRAMA, COMEDIA));
+CREATE TABLE tbl_titulo (codigo_titulo SERIAL PRIMARY KEY, titulo TEXT NOT NULL, descricao TEXT, categoria TEXT CHECK (categoria IN ('DRAMA', 'COMEDIA')));
 
-create table tbl_emprestimo(numero_emprestimo INTEGER PRIMARY KEY, codigo_cliente FOREIGN KEY, codigo_livro FOREIGN KEY);
+CREATE TABLE tbl_livros (codigo_livro SERIAL PRIMARY KEY, codigo_titulo INTEGER REFERENCES tbl_titulo(codigo_titulo), status TEXT CHECK (status IN ('DISPONIVEL', 'ALUGADO')));
 
-create table tbl_livros(codigo_livro INTEGER PRIMARY KEY, codigo_titulo FOREIGN KEY, status IN(DISPONIVEL, ALUGADO));
-
-/* drop database IF EXISTS bd_aula03;
-
-create database bd_aula03;
-
-\c bd_aula03
-
-create table tbl_cliente(codigo_cliente INTEGER PRIMARY KEY, Nome NOTNULL, Cidade text, Endereco text);
-
-create table tbl_titulo(codigo_titulo integer PRIMARY KEY, titulo NOTNULL, descricao text);
-
-create table tbl_emprestimo(numero_emprestimo integer PRIMARY KEY, FOREIGN KEY(codigo_cliente, codigo_livro) );
-
-create table tbl_livros(codigo_livro integer PRIMARY KEY, codigo_titulo FOREIGN KEY);*/
+CREATE TABLE tbl_emprestimo (numero_emprestimo SERIAL PRIMARY KEY, codigo_cliente INTEGER REFERENCES tbl_cliente(codigo_cliente), codigo_livro INTEGER REFERENCES tbl_livros(codigo_livro));
 
