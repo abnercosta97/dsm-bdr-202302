c410ebf5e90285504dc6cf4b9e7decc402c3191d
abnercosta97 <abner.costa3@fatec.sp.gov.br>
2023-08-18 23:08:08 -0300

    correcao

--- aula03.sql
+++ aula03.sql
@@ -3,11 +3,29 @@ DROP DATABASE IF EXISTS bd_aula03;
 CREATE DATABASE bd_aula03;
 \c bd_aula03;
 
-CREATE TABLE tbl_cliente (codigo_cliente SERIAL PRIMARY KEY, Nome TEXT NOT NULL,Cidade TEXT,Endereco TEXT);
+CREATE domain chk_status text check (value = 'Disponivel' or value = 'Alugado');
 
-CREATE TABLE tbl_titulo (codigo_titulo SERIAL PRIMARY KEY, titulo TEXT NOT NULL, descricao TEXT, categoria TEXT CHECK (categoria IN ('DRAMA', 'COMEDIA')));
+CREATE domain chk_categoria text check (value = 'Drama' or value = 'Comedia');
 
-CREATE TABLE tbl_livros (codigo_livro SERIAL PRIMARY KEY, codigo_titulo INTEGER REFERENCES tbl_titulo(codigo_titulo), status TEXT CHECK (status IN ('DISPONIVEL', 'ALUGADO')));
+CREATE TABLE tbl_cliente (codigo_cliente integer PRIMARY KEY, Nome TEXT NOT NULL, Cidade TEXT, Endereco TEXT);
 
-CREATE TABLE tbl_emprestimo (numero_emprestimo SERIAL PRIMARY KEY, codigo_cliente INTEGER REFERENCES tbl_cliente(codigo_cliente), codigo_livro INTEGER REFERENCES tbl_livros(codigo_livro));
+CREATE TABLE tbl_titulo (codigo_titulo integer PRIMARY KEY, titulo TEXT NOT NULL, descricao TEXT);
 
+CREATE TABLE tbl_livros (codigo_livro integer PRIMARY KEY, codigo_titulo INTEGER REFERENCES tbl_titulo(codigo_titulo));
+
+CREATE TABLE tbl_emprestimo (numero_emprestimo integer PRIMARY KEY, codigo_cliente INTEGER REFERENCES tbl_cliente(codigo_cliente), codigo_livro INTEGER REFERENCES tbl_livros(codigo_livro));
+
+alter table tbl_titulo add column categoria chk_categoria;
+
+alter table tbl_livros add column status chk_status;
+
+
+/*
+CREATE TABLE tbl_cliente (codigo_cliente integer PRIMARY KEY, Nome TEXT NOT NULL,Cidade TEXT,Endereco TEXT);
+
+CREATE TABLE tbl_titulo (codigo_titulo integer PRIMARY KEY, titulo TEXT NOT NULL, descricao TEXT, categoria TEXT CHECK (categoria IN ('DRAMA', 'COMEDIA')));
+
+CREATE TABLE tbl_livros (codigo_livro integer PRIMARY KEY, codigo_titulo INTEGER REFERENCES tbl_titulo(codigo_titulo), chk_status TEXT CHECK (chk_status IN ('DISPONIVEL', 'ALUGADO')));
+
+CREATE TABLE tbl_emprestimo (numero_emprestimo integer PRIMARY KEY, codigo_cliente INTEGER REFERENCES tbl_cliente(codigo_cliente), codigo_livro INTEGER REFERENCES tbl_livros(codigo_livro));
+*/
